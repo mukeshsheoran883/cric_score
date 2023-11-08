8c8dd519eba9ec3c4a6e9daf6116d55f958d3da7
MukeshSheoran <mukeshsheoran883@gmail.com>
2023-11-08 11:05:52 +0530

    ApiEndPoint  commit

---
+++ lib/util/api_end_point.dart
@@ -0,0 +1,8 @@
+class ApiEndPoint {
+  static const String baseUrl =
+      'https://api.cricapi.com/v1/currentMatches?apikey=5530196f-c856-4256-9d8d-956aea57f415&offset=0';
+
+  static String getInfo() {
+    return baseUrl;
+  }
+}
